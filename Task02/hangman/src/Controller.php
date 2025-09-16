b24855333378d3ec651c6f392af761a780b5eb59
Aidar Yunusov <rinatka70@mail.ru>
2025-09-16 17:30:50 +0300

    Delete Task02/hangman directory

--- Task02/hangman/src/Controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace aidar555\hangman\Controller;
-
-use aidar555\hangman\View;
-
-function startGame() {
-    View\showStartScreen();
-}
--- Task02/hangman/src/View.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace aidar555\hangman\View;
-
-function showStartScreen() {
-    echo "Добро пожаловать в игру hangman!\n";
-}
